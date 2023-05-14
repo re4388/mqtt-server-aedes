aec1f0245638088cbd13f9133dacc7048de8a0b9
ben.hu <ben.hu@wemoscooter.com>
2023-05-14 13:14:40 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,59 @@
-import { add } from "./util/adder";
+import Aedes from "aedes";
+import { createServer } from "net";
 
-let res = add(2, 3);
-console.log("res", res);
+const aedes = new Aedes();
+const server = createServer(aedes.handle);
+
+// emitted when a client connects to the broker
+aedes.on("client", function (client) {
+  console.log(
+    `CLIENT_CONNECTED : MQTT Client ${
+      client ? client.id : client
+    } connected to aedes broker ${aedes.id}`
+  );
+});
+// emitted when a client disconnects from the broker
+aedes.on("clientDisconnect", function (client) {
+  console.log(
+    `CLIENT_DISCONNECTED : MQTT Client ${
+      client ? client.id : client
+    } disconnected from the aedes broker ${aedes.id}`
+  );
+});
+// emitted when a client subscribes to a message topic
+aedes.on("subscribe", function (subscriptions, client) {
+  console.log(
+    `TOPIC_SUBSCRIBED : MQTT Client ${
+      client ? client.id : client
+    } subscribed to topic: ${subscriptions
+      .map((s) => s.topic)
+      .join(",")} on aedes broker ${aedes.id}`
+  );
+});
+// emitted when a client unsubscribes from a message topic
+aedes.on("unsubscribe", function (subscriptions, client) {
+  console.log(
+    `TOPIC_UNSUBSCRIBED : MQTT Client ${
+      client ? client.id : client
+    } unsubscribed to topic: ${subscriptions.join(",")} from aedes broker ${
+      aedes.id
+    }`
+  );
+});
+// emitted when a client publishes a message packet on the topic
+aedes.on("publish", function (packet, client) {
+  if (client) {
+    console.log(
+      `MESSAGE_PUBLISHED : MQTT Client ${
+        client ? client.id : "AEDES BROKER_" + aedes.id
+      } has published message "${packet.payload}" on ${
+        packet.topic
+      } to aedes broker ${aedes.id}`
+    );
+  }
+});
+
+const port = 1888;
+server.listen(port, function () {
+  console.log("server started and listening on port ", port);
+});
