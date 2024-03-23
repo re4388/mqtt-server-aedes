99f3bd17883f77d9dd815f23a2ee75d35021e121
re4388 <re4388@gmail.com>
2024-03-23 10:09:16 +0800

    WIP

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import Aedes from "aedes";
 import { createServer } from "net";
 
 const aedes = new Aedes();
+
+// mqtt over tcp
 const server = createServer(aedes.handle);
 
-// emitted when a client connects to the broker
+// 監聽 client 連線事件
 aedes.on("client", function (client) {
   console.log(
     `CLIENT_CONNECTED : MQTT Client ${
@@ -12,7 +14,9 @@ aedes.on("client", function (client) {
     } connected to aedes broker ${aedes.id}`
   );
 });
-// emitted when a client disconnects from the broker
+
+
+// 監聽 client 斷線事件
 aedes.on("clientDisconnect", function (client) {
   console.log(
     `CLIENT_DISCONNECTED : MQTT Client ${
@@ -20,7 +24,9 @@ aedes.on("clientDisconnect", function (client) {
     } disconnected from the aedes broker ${aedes.id}`
   );
 });
-// emitted when a client subscribes to a message topic
+
+
+// 監聽 client 訂閱事件
 aedes.on("subscribe", function (subscriptions, client) {
   console.log(
     `TOPIC_SUBSCRIBED : MQTT Client ${
@@ -30,7 +36,9 @@ aedes.on("subscribe", function (subscriptions, client) {
       .join(",")} on aedes broker ${aedes.id}`
   );
 });
-// emitted when a client unsubscribes from a message topic
+
+
+// 監聽 client 取消訂閱事件
 aedes.on("unsubscribe", function (subscriptions, client) {
   console.log(
     `TOPIC_UNSUBSCRIBED : MQTT Client ${
@@ -40,7 +48,9 @@ aedes.on("unsubscribe", function (subscriptions, client) {
     }`
   );
 });
-// emitted when a client publishes a message packet on the topic
+
+
+// 監聽 client 發佈訊息事件
 aedes.on("publish", function (packet, client) {
   if (client) {
     console.log(
